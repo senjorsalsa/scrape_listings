18da69cb6ba71d73cfcd4d924b76a175358801f0
Victor <67063010+senjorsalsa@users.noreply.github.com>
2024-10-14 01:31:38 +0200

    Update renting_research.py
    Cleaning up and adding some comments

--- renting_research.py
+++ renting_research.py
@@ -4,20 +4,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import re
 
-regex_pattern = r'^\$[\d,]+'
-
+REGEX = r'^\$[\d,]+'
 FORM_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSexOUrOdrp1igZ1gMsLm8hwvPvfgKqkKqxFMyxrX6sK-itWlQ/viewform?usp=sf_link"
-response = requests.get("https://appbrewery.github.io/Zillow-Clone/")
+SCRAPE_LINK = "https://appbrewery.github.io/Zillow-Clone/"
+
+# Fetching the scrape target and souping it
+response = requests.get(SCRAPE_LINK)
 soup = BeautifulSoup(response.text, "html.parser")
 
 all_addresses = soup.find_all("address")
 address_list = [address.get_text().strip().replace('|', '') for address in all_addresses]
 
 all_prices = soup.find_all(class_='PropertyCardWrapper__StyledPriceLine')
-price_list = [re.match(regex_pattern, price.text).group() for price in all_prices]
+
+price_list = [re.match(REGEX, price.text).group() for price in all_prices]
+
 all_links = soup.find_all(class_="property-card-link")
 links_list = [link['href'] for link in all_links]
 
+# Open Chrome with Selenium and prepare to submit to form
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
@@ -25,6 +30,7 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get(FORM_LINK)
 driver.implicitly_wait(4)
 
+# Submitting all listings to the form one by one
 for i in range(len(address_list)):
     address_input = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
     address_input.click()
@@ -49,3 +55,6 @@ for i in range(len(address_list)):
     print(f"Submitted {i + 1}/{len(address_list)}")
 
     driver.implicitly_wait(2)
+
+# Exit Chrome, closing all open tabs
+driver.quit()
