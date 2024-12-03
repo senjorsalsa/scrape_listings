2162b4ca10d238bcc245656059f6003e961d85f9
Victor <67063010+senjorsalsa@users.noreply.github.com>
2024-12-03 16:06:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from tkinter import *
 from scraper import scrape_main
 
 window = Tk()
-window.title("Hitta lägenheter")
-window.config(padx=20, pady=20)
-window.geometry("280x200")
+window.title("Find listings")
+window.config(padx=10, pady=10)
+window.geometry("280x160")
+
 
 def button_clicked():
     scrape_main(var1, var2, var3, var4)
+
 
 var1 = IntVar()
 var2 = IntVar()
@@ -26,7 +28,10 @@ heim_check.grid(row=2, column=0, sticky="w")
 boplats_check = Checkbutton(text="Boplats Syd", variable=var4, onvalue=1, offvalue=0)
 boplats_check.grid(row=3, column=0, sticky="w")
 
-submit_button = Button(text="Hämta lägenheter", command=button_clicked)
-submit_button.grid(row=5, column=0, pady=10)
+submit_button = Button(text="Get listings", command=button_clicked)
+submit_button.grid(row=5, column=0, padx=5, pady=10)
+
+quit_button = Button(text="Exit", command=window.destroy)
+quit_button.grid(row=5, column=1, padx=10)
 
 window.mainloop()
